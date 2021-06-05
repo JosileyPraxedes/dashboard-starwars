762c608fb4ea1f64c1f2d37e730b7eea089abbf8
Josiley Cesario <josileycpraxedes@gmail.com>
2021-06-05 14:56:42 -0300

    formatando tabela

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const planetasContator = document.getElementById("planetas");
 const luasContator = document.getElementById("luas");
 
 personagensContatores();
+preencherTabela();
 
 function personagensContatores() {
     Promise.all([
@@ -14,7 +15,6 @@ function personagensContatores() {
         swapiGet("planets/"),
         swapiGet("vehicles/")
     ]).then(function(results) {
-        console.log(results);
 
         personagensContator.innerHTML = results[0].data.count;
         navesContator.innerHTML = results[1].data.count;
@@ -25,11 +25,46 @@ function personagensContatores() {
 
 // maneira 2
 async function preencherTabela() {
-    const responde = await swapiGet("films/");
-    console.log(response);
+    
+    const response = await swapiGet("films/");
+    const tableData = response.data.results;
+    tableData.forEach((film) => {
+        $("#filmsTable").append(`<tr>
+        <td>${film.title}</td>
+        <td>${moment(film.release_date).format("DD/MM/YYYY")}</td>
+        <td>${film.director}</td>
+        <td>${film.episode_id}</td>
+        </tr>`);
+    });
 }
 
 function swapiGet(param) {
-    // Make a request for a user with a given ID
-    return axios.get(`https://swapi.dev/api/${param}`);
+return axios.get(`https://swapi.dev/api/${param}`);
+}
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(desenharGrafico);
+
+async function desenharGrafico() {
+    const response = await swapiGet('vehicles/');
+    const vehiclesArray = response.data.results;
+    console.log(vehiclesArray)
+
+    const dataArray = [];
+    dataArray.push(["Veículos", "Passageiros"]);
+    vehiclesArray.forEach(vehicles =>{
+        dataArray.push([vehicles.name, Number(vehicles.passengers)])
+    });
+
+    var dat
+
+    var data = google.visualization.arrayToDataTable(dataArray);
+
+    var options = {
+        title: 'My Daily Activities'
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+    chart.draw(data, options);
 }
